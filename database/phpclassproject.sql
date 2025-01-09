f0555e09dad890c2e0f9e6924714cebaf6d3037e
Sharjeel khan <iamkhansharjeel@gmail.com>
2025-01-09 11:17:56 +0500

    updated

--- database/phpclassproject.sql
+++ database/phpclassproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 16, 2024 at 08:30 AM
+-- Generation Time: Jan 09, 2025 at 07:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
+  `ctid` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `image` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -37,12 +37,52 @@ CREATE TABLE `categories` (
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `name`, `image`) VALUES
-(1, 'Moblie Phone', 'book1.jpg'),
-(3, 'Book', 'book3.jpg'),
-(5, 'Shirts', 'book5.jpg'),
-(6, 'Paints', 'book8.jpg'),
-(7, 'Electronics', 'book9.jpg');
+INSERT INTO `categories` (`ctid`, `name`, `image`) VALUES
+(9, 'Mens', 'banner-02.jpg'),
+(10, 'Womens', 'banner-01.jpg'),
+(11, 'Accessories', 'banner-03.jpg'),
+(12, 'Bags', 'banner-08.jpg'),
+(13, 'Watches', 'banner-07.jpg'),
+(14, 'Kids', 'banner-06.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `invoiceid` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `useremail` varchar(50) NOT NULL,
+  `totalproductquantity` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  `invoicedate` date NOT NULL,
+  `invoicetime` time NOT NULL,
+  `userphone` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `useremail` varchar(100) NOT NULL,
+  `userphone` varchar(15) NOT NULL,
+  `useraddress` varchar(200) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `productname` varchar(50) NOT NULL,
+  `productprice` int(11) NOT NULL,
+  `productquantity` int(11) NOT NULL,
+  `productimage` varchar(200) NOT NULL,
+  `orderdate` date NOT NULL,
+  `ordertime` time NOT NULL,
+  `orderstatus` varchar(20) NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -65,11 +105,37 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `quantity`, `description`, `image`, `categoryid`) VALUES
-(9, 'Iphone', '$200000', '350', 'sasasasafvgdss', 'book11.jpg', 5),
-(12, 'White Jeans', '$300', '15', 'asasadasasa', 'book10.jpg', 3),
-(13, 'Bulb1', '$500', '50', 'sasasa\r\n', 'clinic4-home-icon-1.png', 7),
-(14, 'T_Shirt', '1500000', '20', 'adsdsfc', 'book8.jpg', 3),
-(17, 'Yoga ', '$1500', '5', 'asasas', 'book3.jpg', 6);
+(24, 'Lightweight Jacket', '58.79', '5', 'Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.', 'product-detail-01.jpg', 9),
+(25, 'Cotton Suit', '1500', '20', 'asasasasasas', 'product-02.jpg', 10),
+(26, 'Smart Watch', '200', '200', 'sasasasasasa', 'product-06.jpg', 13),
+(27, 'Belt', '15', '5', 'vcvcvcvcv', 'product-12.jpg', 11),
+(28, 'School Bag', '150', '350', 'asasasasas', 'gallery-01.jpg', 12),
+(29, 'Shirt', '200', '20', 'dsdsdsdsd', 'product-14.jpg', 14);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_password` varchar(200) NOT NULL,
+  `user_phone` varchar(11) NOT NULL,
+  `user_role_type` varchar(50) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_phone`, `user_role_type`) VALUES
+(3, 'Sharjeel khan', 'Iamkhansharjeel@gmail.com', '$2y$10$c2w8U122Hbgxzv9IciOat.6VXATaihtW3HKRri2TGOsCjVp8qMFqm', '03033851771', 'user'),
+(4, 'Hussnain', 'hussnian@gmail.com', '$2y$10$dXX1mdEAyun5Jv7MTGxyWOokg6AwVWAmQ.rD9Cd2.i.CvAgGAD3U6', '03093739770', 'user'),
+(5, 'Touqeer', 'Touqeer@gmail.com', '$2y$10$X5xjmJJnHBfqtaH532lUruXs4AK3aKD4oSyRvfE4yzaPDDxIVKnW2', '03093739770', 'user'),
+(7, 'ADMIN', 'admin@gmail.com', '$2y$10$CL2jhflfTxIhSBGevVy3MeWqa7fbcUeiw8f9KmAyca59sB0TrB50W', '03033851771', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -79,7 +145,19 @@ INSERT INTO `products` (`id`, `name`, `price`, `quantity`, `description`, `image
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`ctid`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`invoiceid`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderid`);
 
 --
 -- Indexes for table `products`
@@ -89,6 +167,12 @@ ALTER TABLE `products`
   ADD KEY `categoryid` (`categoryid`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -96,13 +180,31 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ctid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `invoiceid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -112,9 +214,9 @@ ALTER TABLE `products`
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `fk_products_category` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`id`),
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`id`),
-  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`id`);
+  ADD CONSTRAINT `fk_products_category` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`ctid`),
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`ctid`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`ctid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
